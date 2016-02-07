35d1931a7a85d501ef3a9790e1b68d5dd72fca58
Evgenia Manolova <emanolova@gmail.com>
2016-02-07 17:12:22 +0100

    some refactiring and fixed the activities service

--- js/home/homeController.js
+++ js/home/homeController.js
@@ -1,21 +1,27 @@
-bmg.controller('homeController', ['$scope', 'initializer', 'utils', '$translate', function ($scope, initializer, utils, $translate) {
-  $scope.activities = utils.activities;
+bmg.controller('homeController', ['$scope', 'initializer', 'utils', '$http', function ($scope, initializer, utils, $http) {
+
+  $scope.activities = [];
+
+  console.log('Getting the activities list..');
+
+  $http.get('http://api.bemyguide-dev.com:3000/activities.json')
+  .success(function(data) {
+    $scope.activities = data;
+    console.log($scope.activities.length + ' activities obtained.');
+  })
+  .error(function (error) {
+    console.log('Error while getting activities list! ' + error);
+    $scope.activities = utils.ActivitiesFallbackData;
+  });
+
+//  utils.loadActivities();
+
   $scope.initializeIntro = initializer.initializeIntro;
   $scope.initializeSearch = initializer.initializeSearch;
   $scope.initializeHowItWorks = initializer.initializeHowItWorks;
   $scope.pickActivity = initializer.pickActivity;
 
-  $scope.getCurrentLanguage = function getCurrentLanguage() {
-    return $translate.use() ||
-    $translate.storage().get($translate.storageKey()) ||
-    $translate.preferredLanguage();
-  };
-
-  $scope.getLanguages = function getLanguages() {
-    return languageKeys;
-  };
-
-  $scope.changeLanguage = function (langKey) {
-    $translate.use(langKey);
-  };
+  $scope.getCurrentLanguage = utils.getCurrentLanguage;
+  $scope.getLanguages = utils.getLanguages;
+  $scope.changeLanguage = utils.changeLanguage;
 }]);
---
+++ js/utils/utils.js
@@ -0,0 +1,43 @@
+bmg.factory('utils', ['$translate', function ($translate) {
+  "use strict";
+
+  var factory = {};
+
+  factory.ActivitiesFallbackData = [
+    { name: 'biking', translation_key: 'biking' },
+    { name: 'cannoeing', translation_key: 'cannoeing' },
+    { name: 'canyoning', translation_key: 'canyoning' },
+    { name: 'caving', translation_key: 'caving' },
+    { name: 'climbing', translation_key: 'climbing' },
+    { name: 'cycling', translation_key: 'cycling' },
+    { name: 'high_montain', translation_key: 'high_montain' },
+    { name: 'hiking', translation_key: 'hiking' },
+    { name: 'ice_climbing', translation_key: 'ice_climbing' },
+    { name: 'kayaking', translation_key: 'kayaking' },
+    { name: 'paragliding', translation_key: 'paragliding' },
+    { name: 'rafting', translation_key: 'rafting' },
+    { name: 'scuba diving', translation_key: 'scuba' },
+    { name: 'ski touring', translation_key: 'ski_touring' },
+    { name: 'skydiving', translation_key: 'skydiving' },
+    { name: 'snowshoeing', translation_key: 'snowshoeing' },
+    { name: 'wind_surfing', translation_key: 'wind_surfing' },
+    { name: 'kite_surfing', translation_key: 'kite_surfing' },
+    { name: 'trekking', translation_key: 'trekking' }
+  ];
+
+  factory.getCurrentLanguage = function getCurrentLanguage() {
+    return $translate.use() ||
+    $translate.storage().get($translate.storageKey()) ||
+    $translate.preferredLanguage();
+  };
+
+  factory.getLanguages = function getLanguages() {
+    return languageKeys;
+  };
+
+  factory.changeLanguage = function changeLanguage(langKey) {
+    $translate.use(langKey);
+  };
+
+  return factory;
+}]);
