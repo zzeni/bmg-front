be13e30e466572716c18e342653d3e1f0a376d1d
Evgenia Manolova <emanolova@gmail.com>
2016-05-10 03:04:52 +0300

    start using the new api and web backend

--- dist/bmg-front.js
+++ dist/bmg-front.js
@@ -178,9 +178,9 @@ app.factory('utils', ['$log', '$translate', 'api', 'config', '$filter', function
   factory.loadCountries = function loadActivities(scope) {
     $log.debug('Retrieving the list of activities ..');
 
-    api.get('/countries.json')
+    api.get('/countries')
     .success(function(data) {
-      scope.countries = data;
+      scope.countries = data.data;
       $log.debug(scope.countries.length + ' countries obtained.');
     })
     .error(function (error) {
@@ -191,17 +191,16 @@ app.factory('utils', ['$log', '$translate', 'api', 'config', '$filter', function
   factory.loadActivities = function loadActivities(scope) {
     $log.debug('Retrieving the list of activities ..');
 
-    api.get('/activities.json')
+    api.get('/activities')
     .success(function(data) {
-      var activities = [];
-      angular.forEach(data, function (el) {
+      var activities = data.data;
+      activities.forEach(function (el) {
         $translate('utils.activities.' + el.translation_key).then(function (translation) {
           el.title = translation;
         });
         $translate('utils.activity_categories.' + el.category).then(function (translation) {
           el.category_title = translation;
         });
-        activities.push(el);
       });
       scope.activities = $filter('orderBy')(activities, '+category');
       $log.debug(scope.activities.length + ' activities obtained.');
